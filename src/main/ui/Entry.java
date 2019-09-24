94b7db06cb12b1ed4e4360527e35a2f1d67ee660
MaggieW <maggiewang759@gmail.com>
2019-09-23 19:46:41 -0700

    Added tests

--- src/main/ui/Entry.java
+++ src/main/ui/Entry.java
@@ -1,23 +1,48 @@
 package ui;
 
+// represents an entry in a list
 public class Entry {
     private int number;
-    private String title;
+    private String item;
+    private String status;
 
-    public Entry(String title) {
+    // MODIFIES: this
+    // EFFECTS: creates an entry with number of 0 and takes a string as a parameter which becomes the item
+    public Entry(String item) {
         number = 0;
-        this.title = title;
+        this.item = item;
+        status = "not done";
     }
 
+    // MODIFIES: this
+    // EFFECTS: sets the number of the entry to an int number
     public void setNumber(int number) {
         this.number = number;
     }
 
-    public String todoGetItem() {
-        return number + " " + title + " to do";
+    //public void setStatus(Entry entry, String status) {
+      //  entry.status = status;
+    //}
+
+    public int getNumber() {
+        return number;
     }
 
-    public String crossedOffGetItem() {
-        return number + " " + title + " crossed off";
+    public String getItem() {
+        return item;
+    }
+
+    // REQUIRES: the entry is in the todo list
+    // EFFECTS: returns the number and item of the entry
+    public String todoGetEntry() {
+
+        return number + " " + item + " to do";
+    }
+
+    // REQUIRES: the entry is in the crossedOff list
+    // EFFECTS: returns the number and item of the entry
+    public String crossedOffGetEntry() {
+
+        return number + " " + item + " crossed off";
     }
 }
--- src/main/ui/TodoList.java
+++ src/main/ui/TodoList.java
@@ -4,11 +4,20 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TodoList {
-    private static ArrayList<Entry> todo = new ArrayList<>();
-    private static ArrayList<Entry> crossedOff = new ArrayList<>();
+    private static ArrayList<Entry> todo;
+    private static ArrayList<Entry> crossedOff;
     private static Scanner scanner = new Scanner(System.in);
 
+    public TodoList() {
+        todo = new ArrayList<>();
+        crossedOff = new ArrayList<>();
+    }
+
+    // run the program
+    // MODIFIES: this
+    // EFFECTS:
     public static void main(String[] args) {
+        TodoList todo = new TodoList();
         while (true) {
             System.out.println("what would you like to do [1] add a to do list item, [2] cross off an item [3] "
                     + "show all the items");
@@ -16,34 +25,52 @@ public class TodoList {
             scanner = new Scanner(System.in);
             if (choice == 1) {
                 System.out.println("Enter the item text.");
-                addTodo();
+                Entry entry = new Entry(scanner.next());
+                todo.addTodo(entry);
             } else if (choice == 2) {
                 System.out.println("Which item would you like to cross off?");
-                moveItem();
+                int removing = scanner.nextInt();
+                todo.moveItem(removing);
             } else {
-                printLists();
+                todo.printLists();
             }
         }
     }
 
-    public static void addTodo() {
-        Entry entry = new Entry(scanner.next());
-        entry.setNumber(todo.size() + crossedOff.size() + 1);
+    // REQUIRES: user has chosen to add an item to the todo list
+    // MODIFIES: this
+    // EFFECTS: adds an entry into the todo list consisting of the item and its number
+    public void addTodo(Entry e) {
+        e.setNumber(todo.size() + crossedOff.size() + 1);
+        todo.add(e);
+    }
+
+    // EFFECTS: prints the todo list and then the crossed out list
+    public void printLists() {
+        for (Entry e : todo) {
+            System.out.println(e.todoGetEntry());
+        }
+        for (Entry e : crossedOff) {
+            System.out.println(e.crossedOffGetEntry());
+        }
+    }
+
+     //MODIFIES: this
+     //EFFECTS: moves the selected item from the todo list to the crossedOff list
+    public void moveItem(int removing) {
+        crossedOff.add(todo.get(removing - 1));
+        todo.remove(removing - 1);
+    }
+
+    public int size() {
+        return todo.size();
+    }
+
+    public void insert(Entry entry) {
         todo.add(entry);
     }
 
-    public static void printLists() {
-        for (Entry e : todo) {
-            System.out.println(e.todoGetItem());
-        }
-        for (Entry e : crossedOff) {
-            System.out.println(e.crossedOffGetItem());
-        }
-    }
-
-    public static void moveItem() {
-        int removing = scanner.nextInt();
-        crossedOff.add(todo.get(removing - 1));
-        todo.remove(removing - 1);
+    public boolean contains(Entry entry) {
+        return todo.contains(entry);
     }
 }
