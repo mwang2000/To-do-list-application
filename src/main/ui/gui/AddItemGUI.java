4d6da16aba57d9e3a47c8d8308011ba7a29d9208
MaggieW <maggiewang759@gmail.com>
2020-05-09 22:34:59 -0700

    agfd

--- src/main/ui/gui/AddItemGUI.java
+++ src/main/ui/gui/AddItemGUI.java
@@ -16,24 +16,6 @@ public class AddItemGUI implements ActionListener {
     private Item item;
     private TodoList todo;
     private JFrame frame;
-    // EFFECTS: shows JOptionPane input dialogs asking user for task and due date and sets the item's fields accordingly
-    // then calls a method to add the item to the given to do list, or asks user to try again if due date fields are not
-    // integers
-//    public AddItemGUI(TodoList todo, Item item) {
-//        try {
-//            String task = JOptionPane.showInputDialog("Enter a task");
-//            int year = Integer.parseInt(JOptionPane.showInputDialog("Enter the year due"));
-//            int month = Integer.parseInt(JOptionPane.showInputDialog("Enter the month due"));
-//            int day = Integer.parseInt(JOptionPane.showInputDialog("Enter the day due"));
-//            item.setTask(task);
-//            item.setDue(year,month,day);
-//            item.setStatus("not done");
-//            todo.addItem(item);
-//        } catch (NumberFormatException e) {
-//            new JOptionPane().showMessageDialog(null,
-//                    "Input must be an integer. Please try again.");
-//        }
-//    }
 
     // EFFECTS: creates new JFrame that asks user for input
     public AddItemGUI(TodoList todo,Item item) {
